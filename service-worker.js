f87dcecf3a49c256f3bf76cfc75759ad495af95d
muhammadzakifoma <146045008+muhammadzakifoma@users.noreply.github.com>
2024-10-03 02:07:28 +0700

    ROOT

--- service-worker.js
+++ service-worker.js
@@ -18,11 +18,12 @@ self.addEventListener('install', event => {
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      }).catch(() => caches.match('/offline.html'))
+      .then((response) => {
+        return response || fetch(event.request).catch(() => caches.match('/offline.html'));
+      })
   );
 });
+
